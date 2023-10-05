738c679f2ac6a01b2eddf1c1475c8ba328bca628
Alejandro Lopez <33030244+alejlopez139@users.noreply.github.com>
2023-10-05 19:14:38 +0000

    passed all tests

--- app/models/character.rb
+++ app/models/character.rb
@@ -10,4 +10,18 @@
 #  movie_id   :integer
 #
 class Character < ApplicationRecord
+
+  def movie
+    my_movie_id = self.movie_id
+    matching_movie = Movie.where({:id => my_movie_id})
+    the_movie = matching_movie.at(0)
+    return the_movie
+  end
+
+  def actor
+    my_actor_id = self.actor_id
+    matching_actor = Actor.where({:id => my_actor_id})
+    the_actor = matching_actor.at(0)
+    return the_actor
+  end
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -13,4 +13,22 @@
 #  director_id :integer
 #
 class Movie < ApplicationRecord
+
+  def director
+    d_id = self.director_id
+
+    matching_director = Director.where({:id => d_id})
+    the_director = matching_director.at(0)
+
+    return the_director
+    
+  end
+
+  def characters
+    my_id = self.id
+
+    matching_characters = Characters.where({:movie_id => my_id})
+
+    return matching_characters
+  end
 end
